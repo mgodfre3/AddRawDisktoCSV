a93dae869d245911b4ed007b311fe99e19e97a91
mgodfre3 <migodfre@microsoft.com>
2019-07-12 11:35:12 -0400

    Create AddRawDisktoCSV.ps1

---
+++ AddRawDisktoCSV.ps1
@@ -0,0 +1,28 @@
+﻿Function AddRawDisktoCSV{
+
+
+Param(
+[string]$diskname=""
+)
+
+
+$diskinfo=Get-Disk | Where partitionstyle -eq ‘raw’  
+
+Initialize-Disk -Number $diskinfo.number -PartitionStyle GPT -PassThru -Verbose
+
+Start-Sleep -Seconds 15
+
+Write-Host "Creating Partition on "$diskinfo.friendlyname""
+ 
+New-Partition -DiskNumber $diskinfo.Number -UseMaximumSize  -WarningAction Continue| Format-Volume -FileSystem NTFS -NewFileSystemLabel $diskname -Confirm:$false -Verbose
+
+Get-ClusterAvailableDisk | Add-ClusterDisk| Add-ClusterSharedVolume $diskname -Verbose 
+
+Get-ClusterSharedVolume | ?{$_.Name -eq "Cluster Disk 1"} | %{$_.Name = $diskname}
+
+Rename-Item -Path C:\ClusterStorage\Volume1 -NewName C:\ClusterStorage\"$diskname"
+
+
+}
+
+
